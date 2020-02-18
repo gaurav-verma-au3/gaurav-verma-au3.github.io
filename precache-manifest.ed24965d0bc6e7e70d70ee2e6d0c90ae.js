23fb23f0d1db384d959878162f765f8629415b32
gaurav-verma-au3 <gauravve1@gmail.com>
2020-02-19 04:09:51 +0530

    Updates

---
+++ precache-manifest.ed24965d0bc6e7e70d70ee2e6d0c90ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f407ce4e4b73539cacd7be3de29cc8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f68af16573a3c4026bec",
+    "url": "/static/css/2.f6e6f51f.chunk.css"
+  },
+  {
+    "revision": "037221a995b95637186e",
+    "url": "/static/css/main.50491cf9.chunk.css"
+  },
+  {
+    "revision": "f68af16573a3c4026bec",
+    "url": "/static/js/2.0dbc63e0.chunk.js"
+  },
+  {
+    "revision": "fe6dbac30a5209373c4964c692feaa71",
+    "url": "/static/js/2.0dbc63e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037221a995b95637186e",
+    "url": "/static/js/main.60023d42.chunk.js"
+  },
+  {
+    "revision": "029394bcab2de99f2659",
+    "url": "/static/js/runtime-main.413fcbef.js"
+  }
+]);
